817a5be5a7ada0d10b47d2f42d5497347c0e770d
root <root@training-webide-b09d4a-b09d4a-webide-master-0.training-webide-b09d4a-b09d4a-webide-master.dev-kowteozo.svc.cluster.local>
2023-01-21 20:08:14 +0800

    init

---
+++ init.sh
@@ -0,0 +1,16 @@
+pip install openmim
+mim install mmcv-full==1.3.13
+mim install mmdet==2.14.0
+mim install mmsegmentation==0.18.0
+cd mmdetection3d
+pip install -e .
+cd ..
+pip uninstall pycocotools -y
+mim install mmpycocotools
+cd pypcd
+python setup.py install
+# cd ..
+# pip install vtk==8.1.2
+# pip install mayavi
+# apt-get update
+# pip install open3d==0.11.1
