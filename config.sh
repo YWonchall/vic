1a6962df67586136dcdcd21e44dd551d50ec2ad7
lab <ywonchall@163.com>
2023-03-11 19:33:59 +0800

    update config

--- config.sh
+++ config.sh
@@ -1,14 +1,12 @@
-pip install mmcv-full==1.3.13
-pip install mmdet==2.14.0
-pip install mmsegmentation==0.18.0
+pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install openmim
+mim install mmcv-full==1.5.2
+mim install mmdet==2.26.0
+mim install mmsegmentation==0.29.1
 cd mmdetection3d
-pip install -e .
-cd ..
-pip install pycocotools==2.0.1
-cd pypcd
+pip install -v -e .
+cd ../pypcd
 python setup.py install
 cd ..
 pip install seaborn
-# pip install vtk==8.1.2
-# pip install mayavi
 pip install open3d==0.11.1
